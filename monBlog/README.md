f411953d0e9065f23d2cb87a9b97dc0aa4051550
youassy <arseneyouassy90@gmail.com>
2022-08-05 10:10:53 +0100

    initial commit, mise en plate du framework laravel et creation du blog

---
+++ monBlog/README.md
@@ -0,0 +1,14 @@
+## Blog Laravel
+
+## BlogLara est une application devoloppee par Youassy Gilles Arsene
+
+il est question ici de pouvoir poster des blogs au grand public. 
+
+la vision ici est de creer une plate forme ou tout un chacun pourra poster un article s'il voudrait vendre, 
+poster une information s'il voudrait passer une information...
+
+### procedure pour pouvoir poster 
+
+- etre reconnu comme un utilisateur de la plate forme bref s'enregistrer grace au systeme d'authentification
+- creer son poste suivant les regles bien etablies
+- de plus pouvoir le modifier, supprimer qusnt il en a besoin.
---
+++ monBlog/app/Http/Controllers/PostController.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use App\Models\Category;
+use Illuminate\Support\Facades\Gate;
+use App\Http\Requests\StorePostRequest;
+use App\Http\Requests\UpdatePostRequest;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = Post::with('category', 'user')->latest()->get();
+
+        return view('post.index', compact('posts'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $categories = Category::all();
+        
+        return view('post.create', compact('categories'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StorePostRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StorePostRequest $request)
+    {
+        $imageName = $request->image->store('posts');
+
+        Post::create([
+            'title' => $request->title,
+            'content' => $request->content,
+            'image' => $imageName
+
+        ]);
+        return redirect()->route('dashboard')->with('success', 'votre post a ete cree');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Post $post)
+    {
+        return view('post.show', compact('post'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Post $post)
+    {
+        if (! Gate::allows('update-post', $post)) {
+            abort(403);
+        }
+
+        $categories = Category::all();
+        return view('post.edit', compact('post', 'categories'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\Request  $request
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function update(StorePostRequest $request, Post $post)
+    {
+        $arrayUpdate = [
+
+            'title' => $request->title,
+            'content' => $request->content
+        ];
+
+        if ($request->image != null){
+
+            $imageName = $request->image->store('posts');
+            $arrayUpdate = array_merge($arrayUpdate, [
+                'image' => $imageName
+            ]);
+        }
+
+        $post->update($arrayUpdate);
+        return redirect()->route('dashboard')->with('success', 'votre post a ete modifie');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Post $post)
+    {
+        if (! Gate::allows('destroy-post', $post)) {
+            abort(403);
+        }
+
+        $post->delete();
+        return redirect()->route('dashboard')->with('success', 'votre post a ete supprime');
+    }
+}
